4b448423359154087ada73bb6c98d73a08872886
Andres <andres.casado.94@gmail.com>
2016-11-15 13:20:43 +0100

    Added more parameters to solvers

--- GradientDescent/Solver.py
+++ GradientDescent/Solver.py
@@ -8,41 +8,43 @@ class Solver(object):
 
 
 class GradientDescent(Solver):
-    def __init__(self, dF, args, k=0.001, precision=1 / np.power(10, 10)):
+    def __init__(self, dF, args, k=0.001, precision=1 / np.power(10, 10), maxSteps=500):
         self.dF = dF
         self.args = args
         self.k = k
         self.precision = precision
+        self.maxSteps = maxSteps
 
     def solve(self):
         result = self.args
-        keepGoing = True
-        while (keepGoing):
+        for i in range(self.maxSteps):
             value = self.dF(result)
             norm = np.linalg.norm(value)
             if norm < self.precision:
-                keepGoing = False
+                break
             else:
                 result = result - self.k * value
         return result
 
 
 class GaussNewton(Solver):
-    def __init__(self, J, args, k=0.001, precision=1 / np.power(10, 10)):
+    def __init__(self, f, J, args, k=0.001, precision=1 / np.power(10, 10), maxSteps=500):
+        self.f = f
         self.J = J
         self.args = args
         self.k = k
         self.precision = precision
+        self.maxSteps = maxSteps
 
     def solve(self):
         result = self.args
-        keepGoing = True
-        while (keepGoing):
+        for i in range(self.maxSteps):
             matrix = self.J(result)
-            value = result - np.dot(np.linalg.inv(np.dot(matrix.T, matrix)), np.dot(matrix.T, matrix))
-            norm = np.linalg.norm(value)
+            fvalue = self.f(result)
+            value = result - np.dot(np.linalg.pinv(np.dot(matrix.T, matrix)), np.dot(matrix.T, fvalue))
+            norm = np.linalg.norm(fvalue.dot(fvalue))
             if norm < self.precision:
-                keepGoing = False
+                break
             else:
                 result = value
         return result
