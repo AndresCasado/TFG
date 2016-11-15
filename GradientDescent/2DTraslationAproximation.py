1a4853dead7004272e38f7b0a5fc8e5a7f426623
Andres <andres.casado.94@gmail.com>
2016-11-15 13:22:13 +0100

    Working example of 2D point adjustment

--- GradientDescent/2DTraslationAproximation.py
+++ GradientDescent/2DTraslationAproximation.py
@@ -2,32 +2,44 @@ import numpy
 from Solver import *
 
 P = numpy.array((1, 2))
-Q = numpy.array((150, -900))
+Q = numpy.array((150563.456, -9950.5))
 
 
-def T(P, args):
-    return P + args
+def F(P):
+    def function(args):
+        return P + args
+
+    return function
 
 
 def E(P, Q):
     def function(args):
-        return numpy.linalg.norm(T(P, args) - Q)
+        return F(P)(args) - Q
 
     return function
 
 
 def dE(P, Q):
     def function(args):
-        dif = T(P, args) - Q
+        dif = F(P)(args) - Q
         return dif
 
     return function
 
 
-args = np.array([0, 0])
-gradientDescent = GradientDescent(args=args, dF=dE(P, Q), k=0.1)
-gaussNewton = GaussNewton(J=dE(P, Q), args=args)
+def JE(P, Q):
+    def function(args):
+        return np.eye(2)
+
+    return function
+
+
+point = np.array([0, 0])
+
+gradientDescent = GradientDescent(args=point, dF=dE(P, Q), k=0.1)
 result = gradientDescent.solve()
-print("Gradient:" + str(result))
+print("Gradient: " + str(result))
+
+gaussNewton = GaussNewton(f=E(P, Q), J=JE(P, Q), args=point)
 result = gaussNewton.solve()
-print("GaussNewton:" + str(result))
+print("Gauss-Newton: " + str(result))
